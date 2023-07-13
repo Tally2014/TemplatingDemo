d76bdae01b2d303b86f61ac6b185076ff020aeec
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-07-13 23:52:28 +0200

    Inititail Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req,res) => {
+    res.render('home.ejs');
+});
+
+app.get('/cats', (req, res) =>{
+    const cats = [
+        'Blue','Rocky','Monty', 'Stephanie','Winston'
+    ]
+    res.render('cats',{allCats: cats})
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const  { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    }else{
+        res.render('notfound', {subreddit})
+    }
+})
+
+app.get('/rand', (req, res) =>{
+    const num =   Math.floor(Math.random() * 50) + 1;
+    res.render('random', {rand: num});
+})
+
+app.listen(3000,() => {
+    console.log("LISTENING ON PORT 300");
+}); 
